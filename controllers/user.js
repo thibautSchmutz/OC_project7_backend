e4a8f375eebf86cf1e6cc8bb1c0469c30cc45c9c
thibaut schmutz <thibaut.schmu@gmail.com>
2020-10-24 19:50:46 +0200

    :sparkles: (user routes) hash password before saving

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,19 @@
 const db = require("../models");
+const bcrypt = require("bcryptjs");
 
 // @desc      Inscription d'un nouvel utilisateur
 // @route     POST /api/auth/signup
 // @access    Public
 exports.signup = (req, res) => {
-  db.User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-    imageUrl: req.body.imageUrl,
-  }).then((newUser) => res.send(newUser));
+  bcrypt.hash(req.body.password, 6).then((hash) => {
+    db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hash,
+      imageUrl: req.body.imageUrl,
+    })
+      .then((newUser) => res.status(201).send(newUser))
+      .catch((err) => console.log(err));
+  });
 };
