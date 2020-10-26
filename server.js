79eaabbf9f7762b4b6c8003376a7449b608185cf
thibaut schmutz <thibaut.schmu@gmail.com>
2020-10-26 20:30:11 +0100

    :zap: (faille XSS) add xss filter

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const helmet = require("helmet");
 
 // chargement des variables d'environnement
 dotenv.config({ path: "./config/.env" });
 
 const db = require("./models");
 const PORT = process.env.PORT || 5000;
+
+// Ajout des paramètres de securités au headers des responses (contient une méthode xssFilter()).
+app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
